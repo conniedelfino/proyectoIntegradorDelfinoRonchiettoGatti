a6c6a84de7b0dca7df1f16c60b1228c49f6face3
conniedelfino <cdelfinochevallier@udesa.edu.ar>
2025-11-21 11:51:59 -0300

    logout errores

--- js/login.js
+++ js/login.js
@@ -1,28 +1,20 @@
 let formLogin = document.querySelector(".loginForm");
+let emailLogin = document.querySelector("#email");
+let contraLogin = document.querySelector("#password");
 
-formLogin.addEventListener("submit", function(e){
-    let emailLogin = document.querySelector(".emailForm");
-    let contraLogin = document.querySelector(".contrasenaForm");
-    let nombre = localStorage.getItem("usuario");
-    let contra = localStorage.getItem("password");
-
-    let email = emailLogin.value;
-    let password = contraLogin.value;
+formLogin.addEventListener("submit", function(evento){
     evento.preventDefault();
-    if(email === "" || password === ""){
-        alert("Los campos son obligatorios!");
-        e.preventDefault();
+    if(emailLogin.value == ""){
+        alert("¡Este campo es obligatorio!");
     }
-    else if (password.length < 6){
-        alert("La contraseña debe contener por lo menos 6 caracteres!");
-        e.preventDefault();
+    else if (contraLogin.value == ""){
+        alert("¡Este campo es obligatorio!");
     }
-    else if (email === nombre && password === contra){
-        localStorage.setItem("logedUser", "True");
-        location.href = "index.html";
+    else if (contraLogin.vaule.length < 6){
+        alert("La contraseña debe tener al menos 6 caracteres");
     }
     else{
-        alert("El usuario o la contraseña son incorrectos, intente nuevamente!")
-        e.preventDefault();
+        localStorage.setItem("userEmail", emailLogin.value);
+        window.location = "index.html";
     }
-})
+});
