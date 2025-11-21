549ac72186a5a31fcc590979e9d5958f7ffb48a9
mronchiettomeilan <mronchiettomeilan  git config --global user.email mronchiettomeilan@udesa.edu.ar>
2025-11-20 22:14:59 -0300

    INDEX(maybe algun error pero creo que listo)

--- js/index.js
+++ js/index.js
@@ -27,3 +27,51 @@ form.addEventListener("submit", function(event){
         this.submit();
     }
 });
+
+let categoria1 = "tops"
+let contenedor1 = document.querySelector(".masvendidos .items");
+
+fetch("https://dummyjson.com/products/category/" + categoria1)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let productos = data.products;
+        for (let i = 0; i < 10; i++){
+            contenedor1.innerHTML += 
+            '<article class="poloproducts">' +
+                '<img src="' + productos[i].thumbnail + '">' +
+                '<h2>' + productos[i].title + '</h2>' +
+                '<p>' + productos[i].brand + '</p>' +
+                '<p>$' + productos[i].price + ' USD</p>' +
+                '<a href="./product.html?id=' + productos[i].id + '">Ver detalle</a>' +
+            '</article>';
+     }
+     })
+     .catch(function(error){
+        console.log("Error: " + error);
+    })
+
+    let categoria2 = "mens-shirts"
+    let contenedor2 = document.querySelector(".aleatorios .items");
+
+fetch("https://dummyjson.com/products/category/" + categoria2)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let productos = data.products;
+        for (let i = 0; i < 10; i++){
+            contenedor2.innerHTML += 
+            '<article class="poloproducts">' +
+                '<img src="' + productos[i].thumbnail + '">' +
+                '<h2>' + productos[i].title + '</h2>' +
+                '<p>' + productos[i].brand + '</p>' +
+                '<p>$' + productos[i].price + ' USD</p>' +
+                '<a href="./product.html?id=' + productos[i].id + '">Ver detalle</a>' +
+            '</article>';
+     }
+     })
+     .catch(function(error){
+        console.log("Error: " + error);
+    })
