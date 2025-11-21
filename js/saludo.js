da9661d7f496479438b7797be6a49cc22224a66e
conniedelfino <cdelfinochevallier@udesa.edu.ar>
2025-11-21 12:44:14 -0300

    cambios saludos

--- js/saludo.js
+++ js/saludo.js
@@ -4,11 +4,8 @@
     let navHead = document.querySelector(".navHead");
 
     if (nombreUsuario){
-
         linkLogin.style.display = "none";
         linkRegistro.style.display = "none";
-
         navHead.innerHTML += `<li class= "elementosheader saludo" >bienvenido: ${userEmail}</li>`
         navHead.innerHTML += `<li class= elementosheader logout><a href = "#">LOGOUT</a></li>`;
-
     };
