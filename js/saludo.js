7d184a50ea7ee06f7a72dbd731d17cad7c7da2b2
conniedelfino <cdelfinochevallier@udesa.edu.ar>
2025-11-21 22:08:53 -0300

    saludo

--- js/saludo.js
+++ js/saludo.js
@@ -1,11 +1,21 @@
-    let nombreUsuario = localStorage.getItem("userEmail");
-    let linkLogin = document.querySelector(".loginB");
-    let linkRegistro = document.querySelector(".registerB");
-    let navHead = document.querySelector(".navHead");
 
-    if (nombreUsuario){
-        linkLogin.style.display = "none";
-        linkRegistro.style.display = "none";
-        navHead.innerHTML += `<li class= "elementosheader saludo" >bienvenido: ${userEmail}</li>`
-        navHead.innerHTML += `<li class= elementosheader logout><a href = "#">LOGOUT</a></li>`;
-    };
+    window.addEventListener("load", function (){
+        let userEmail = localStorage.getItem("userEmail");
+        let linkLogin = document.querySelector(".loginB");
+        let linkRegistro = document.querySelector(".registerB");
+        let navHeadUl = document.querySelector(".navHead ul");
+
+        linkLogin.style.display = "inline-block";
+        linkRegistro.style.display = "inline-block";
+
+        if(userEmail){
+            linkLogin.style.display = "none";
+            linkRegistro.style.display = "none";
+            navHeadUl.innerHTML += `
+            <li class="saludo">Bienvenido: ${userEmail}</li>
+            <li class="logout"><a href="./product.html">Logout</a></li>
+            `;
+
+        }
+
+    });
